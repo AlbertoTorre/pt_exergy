22708621d6bd9fc694313224ef6f9a9f6e98e4c7
Dehost <diego.ochoa@tania.co>
2018-09-03 16:29:15 -0500

    Registro de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +7,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'register'], function () {
+		Route::post('/create', 'Auth\RegisterController@create');
+});
+
+Route::group(['prefix' => 'user', "middleware"=> ['auth'] ], function () {
+		Route::get('/', 'UserController@index');
+		Route::get('/list', 'UserController@getList');
+});
+
+Route::group(['prefix' => 'graphic', "middleware"=> ['auth'] ], function () {
+		Route::get('/random', 'Graphic\ReportController@index');
+});
+
